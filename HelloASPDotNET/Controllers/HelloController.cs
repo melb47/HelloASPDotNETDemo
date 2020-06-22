2f3456c4ccb2101af0d5b1a8cd36490e3fa33989
Melissa B <missmelissaboots@gmail.com>
2020-06-22 14:25:28 -0400

    attempt 1

--- HelloASPDotNET/Controllers/HelloController.cs
+++ HelloASPDotNET/Controllers/HelloController.cs
@@ -17,6 +17,13 @@ namespace HelloASPDotNET.Controllers
         {
             string html = "<form method='post' action='/helloworld'>" +
                 "<input type='text' name='name' />" +
+                "<select name='language'>" +
+                "<option value=''>--Please select a language--</option>" +
+                "<option value='english'>English</option>" +
+                "<option value='french'>French</option>" +
+                "<option value='arabic'>Arabic</option>" +
+                "<option value='spanish'>Spanish</option>" +
+                "<option value='german'>German</option>" +
                 "<input type='submit' value='Greet Me!' />" +
                 "</form>";
 
@@ -27,9 +34,39 @@ namespace HelloASPDotNET.Controllers
         // POST: /<controller>/welcome
         [HttpGet("welcome/{name?}")]
         [HttpPost]
-        public IActionResult Welcome(string name = "World")
+        //public IActionResult Welcome(string name = "World")
+        //{
+        //    return Content("<h1>Welcome to my app, " + name + "!</h1>", "text/html");
+        //}
+
+        // GET: /<controller>/welcome?name=value?language=value or GET: /<controller>/welcome/name/language
+        // POST: /<controller>/index
+        [HttpGet("index")]
+        [HttpPost]
+        public IActionResult CreateMessage(string name = "World", string language = "english")
         {
-            return Content("<h1>Welcome to my app, " + name + "!</h1>", "text/html");
+            string greeting = "";
+            if (language == "english")
+            {
+                greeting = "Hello, ";
+            }
+            if (language == "french")
+            {
+                greeting = "Bonjour, ";
+            }
+            if (language == "arabic")
+            {
+                greeting = "Ahelen Was Ahelen, ";
+            }
+            if (language == "spanish")
+            {
+                greeting = "Hola, ";
+            }
+            if (language == "german")
+            {
+                greeting = "Guttentag, ";
+            }
+            return Content($"<h1>{greeting}{name}!</h1>", "text/html");
         }
     }
 }
